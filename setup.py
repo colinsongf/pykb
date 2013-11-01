48ae4ab80441ce290195d4106cf5311330888fe6
Séverin Lemaignan <severin.lemaignan@epfl.ch>
2013-11-01 13:35:26 +0100

    Complete metadata in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,13 @@ from distutils.core import setup
 
 setup(name='pykb',
       version='1.4',
+      license='BSD',
       description='Python API to access a KB-API conformant knowledge base',
+      classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2.7',
+      ],
+      url='https://github.com/severin-lemaignan/pykb',
       author='Séverin Lemaignan',
       author_email='severin.lemaignan@epfl.ch',
       py_modules=['kb'],
