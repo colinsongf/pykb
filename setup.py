72d99d9b02dad67534bd3f30dce82a9eacdaedd7
Séverin Lemaignan <severin.lemaignan@epfl.ch>
2013-11-01 10:15:42 +0100

    Bumped to 1.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='pykb',
-      version='1.3',
+      version='1.4',
       description='Python API to access a KB-API conformant knowledge base',
       author='Séverin Lemaignan',
       author_email='severin.lemaignan@epfl.ch',
